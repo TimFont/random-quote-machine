ef4859383532bacf57ad930524f6e57da8707ebf
italo fontes <italo5123@hotmail.com>
2019-02-02 17:36:22 -0200

    Updates

---
+++ precache-manifest.568c7a4351d3c172f1a265393f8391c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08a26cf46a1388e5f45f",
+    "url": "/random-quote-machine/static/css/main.2ed5b384.chunk.css"
+  },
+  {
+    "revision": "08a26cf46a1388e5f45f",
+    "url": "/random-quote-machine/static/js/main.08a26cf4.chunk.js"
+  },
+  {
+    "revision": "4ad850479c965bb2b383",
+    "url": "/random-quote-machine/static/js/1.4ad85047.chunk.js"
+  },
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quote-machine/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b405b10a4689feed48c3e4ea706d08d9",
+    "url": "/random-quote-machine/index.html"
+  }
+];
